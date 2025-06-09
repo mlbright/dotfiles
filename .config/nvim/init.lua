d1f2185ee09fb938f0964ad6cdfcbd36801fc394
Martin-Louis Bright <mlbright@gmail.com>
2025-06-08 21:43:51 -0400

    new default colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -292,13 +292,15 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    -- 'folke/industry.nvim',
     'folke/tokyonight.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'industry'
+      -- vim.cmd.colorscheme 'evening'
+      vim.cmd.colorscheme 'koehler'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
