db5317003a903264a4603e37a27238077001aa77
Martin-Louis Bright <mlbright@gmail.com>
2025-09-03 20:28:16 -0400

    do not render markdown

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Completely disable markdown code block rendering
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Clear all syntax highlighting
+    vim.cmd("syntax clear")
+    vim.cmd("syntax off")
+    vim.bo.syntax = ""
+    -- Override any potential highlight groups
+    vim.cmd("highlight! clear markdownCode")
+    vim.cmd("highlight! clear markdownCodeBlock")
+    vim.cmd("highlight! clear markdownCodeDelimiter")
+    vim.cmd("highlight! clear @markup.raw.block.markdown")
+    vim.cmd("highlight! clear @markup.raw.markdown")
+    vim.wo.conceallevel = 0
+  end,
+})
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,7 @@
 return {
-  { "render-markdown.nvim", enabled = false },
-  { "markdown-preview.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  -- Disable any other potential markdown renderers
+  { "OXY2DEV/markview.nvim", enabled = false },
+  { "tadmccorkle/markdown.nvim", enabled = false },
 }
