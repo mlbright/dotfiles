1d548f50672a2c3af738be47698f7f4d60163d41
Martin-Louis Bright <mbright@cpacketnetworks.com>
2026-05-06 09:47:30 -0400

    misc scripts

---
+++ .local/bin/remove-macos-tailscale.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -rf ~/Library/Tailscale
+rm -rf ~/Library/Preferences/io.tailscale.ipn.macsys.plist
+rm -rf ~/Library/Preferences/io.tailscale.ipn.macos.plist
+rm -rf ~/Library/HTTPStorages/io.tailscale.ipn.macsys
+rm -rf ~/Library/HTTPStorages/io.tailscale.ipn.macos
+rm -rf ~/Library/Group Containers/*.io.tailscale.ipn.macsys
+rm -rf ~/Library/Group Containers/*.io.tailscale.ipn.macos
+rm -rf ~/Library/Containers/io.tailscale.ipn.macsys.share-extension
+rm -rf ~/Library/Containers/io.tailscale.ipn.macsys.login-item-helper
+rm -rf ~/Library/Containers/io.tailscale.ipn.macsys
+rm -rf ~/Library/Containers/io.tailscale.ipn.macos.share-extension
+rm -rf ~/Library/Containers/io.tailscale.ipn.macos.network-extension
+rm -rf ~/Library/Containers/io.tailscale.ipn.macos.login-item-helper
+rm -rf ~/Library/Containers/io.tailscale.ipn.macos
+rm -rf ~/Library/Containers/Tailscale
+rm -rf ~/Library/Containers/Tailscale
+rm -rf ~/Library/Caches/io.tailscale.ipn.macsys
+rm -rf ~/Library/Caches/io.tailscale.ipn.macos
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macsys.share-extension
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macsys.login-item-helper
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macsys
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macos.share-extension
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macos.login-item-helper
+rm -rf ~/Library/Application Scripts/io.tailscale.ipn.macos
+rm -rf ~/Library/Application Scripts/*.io.tailscale.ipn.macsys
+rm -rf ~/Library/Application Scripts/*.io.tailscale.ipn.macos
+rm -rf /Library/Tailscale
